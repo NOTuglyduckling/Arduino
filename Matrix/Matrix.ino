ee5526fcfd711c5c7c505f07d1a75de0afbe4224
NOTuglyduckling <osearle65@gmail.com>
2024-11-25 17:03:28 +0100

    Time Keeping
    Added RTC (DS3231) module for precise time keeping.

--- Matrix/Matrix.ino
+++ Matrix/Matrix.ino
@@ -1,23 +1,30 @@
 #include <LedControl.h>
 #include <DHT11.h>
+#include <DS3231.h>
+#include <stdlib.h>
 
+DS3231 myRTC;
+bool h12Flag;
+bool pmFlag;
 #define DATA_IN 12
 #define CLK 11
 #define LOAD 10
 #define ModePin 2
 #define OnOff 3
 #define BRIGHTNESS 0
+#define SPEED 1
+#define RAND_MAX 8
 
 LedControl lc = LedControl(DATA_IN, CLK, LOAD, 1);
 DHT11 dht11(4);
 
-int hours = 17, minutes = 43, seconds = 0; // edit here to set time for now
+int hours = 16, minutes = 19, seconds = 0; // edit here to set time for now
 int h1, h2, m1, m2, t1, t2,hu1,hu2 = 0;
 int temp = 0, humid = 0;
 unsigned long previousMillis = 0;
 const long interval = 1000;
 int Mode = 0, lastButtonState=LOW, lastOnState=LOW;  // Mode toggle for time/temperature/humidity display and On/Off
-unsigned long debounceTime = 200UL, lastDebounce = 0, lastBounce = 0;
+unsigned long debounceTime = 200, lastDebounce = 0, lastBounce = 0;
 bool ScreenState=false;
 
 byte nombre[][4] = {
@@ -37,9 +44,26 @@ byte nombre[][4] = {
   {0x09, 0x0F, 0x09, 0x09}, // H
 };
 
-void resetMatrix() {
+void setup() {
+  lc.shutdown(0, ScreenState); // turn Matrix ON
+  lc.setIntensity(0, BRIGHTNESS); // set brightness of LEDs
   lc.clearDisplay(0);
+  pinMode(ModePin, INPUT);
+  pinMode(OnOff, INPUT);
+  srand(time(NULL));   // Initialization, should only be called once.
+  Wire.begin();
+  myRTC.setClockMode(false);  // set to 24h 
+  myRTC.setYear(2024);
+  myRTC.setMonth(11);
+  myRTC.setDate(25);
+  myRTC.setDoW(1);
+  myRTC.setHour(17);
+  myRTC.setMinute(2);
+  myRTC.setSecond(50);
 }
+
+//############################################################################ END OF SETUP ##################################################################################
+
 
 void updateMatrix(int tl, int tr, int bl, int br) {
   resetMatrix();
@@ -47,6 +71,10 @@ void updateMatrix(int tl, int tr, int bl, int br) {
   displayDigitRotated(tr, 4, 4);
   displayDigitRotated(bl, 0, 0);
   displayDigitRotated(br, 4, 0);
+}
+
+void resetMatrix() {
+  lc.clearDisplay(0);
 }
 
 void displayDigitRotated(int digit, int rowOffset, int colOffset) {
@@ -59,19 +87,13 @@ void displayDigitRotated(int digit, int rowOffset, int colOffset) {
   }
 }
 
-void setup() {
-  lc.shutdown(0, ScreenState); // turn Matrix ON
-  lc.setIntensity(0, BRIGHTNESS); // set brightness of LEDs
-  lc.clearDisplay(0);
-  pinMode(ModePin, INPUT);
-  pinMode(OnOff, INPUT);
-}
+//################################################################################ MAIN ###################################################################################
 
 void loop() {
   int reading = digitalRead(ModePin);
   // Button cycles through options with protection of debounce
   if ((reading == HIGH) && (lastButtonState == LOW) && (millis() - lastDebounce >= debounceTime)) {
-    Mode = (Mode + 1) % 3;
+    Mode = (Mode + 1) % 4;
     lastDebounce = millis();
   }
   lastButtonState=reading;
@@ -84,19 +106,21 @@ void loop() {
     lastBounce = millis();
   }
   lastOnState=read;
-  
+
   unsigned long currentMillis = millis();
   // Matrix is updated every second
   if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+    previousMillis +=1000;
     // increment time variables accordingly
     seconds++;
     if (seconds >= 60) {
       seconds = 0;
       minutes++;
       if (minutes >= 60) {
-        minutes = 0;
-        hours = (hours + 1) % 24;
+        // Every hour update Timekeeping
+        seconds = myRTC.getSecond();
+        minutes = myRTC.getMinute();
+        hours = myRTC.getHour(h12Flag, pmFlag);
       }
     }
 
